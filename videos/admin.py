40dfaabe0b7331fb9bb5f9df8361b487a825c6fc
Tony Wittinger <anwittin@gmail.com>
2017-08-10 21:14:16 -0700

    admin update

--- videos/admin.py
+++ videos/admin.py
@@ -3,4 +3,15 @@ from django.contrib import admin
 # Register your models here.
 from .models import Video
 
-admin.site.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    list_filter = ['title','timestamp', 'updated']
+    list_display = ['title','timestamp', 'updated']
+    readonly_fields = ['updated', 'timestamp', 'short_title']
+    search_fields = ['title']
+    class Meta:
+        model = Video
+
+    def short_title(self, obj):
+        return obj.title[:3]
+
+admin.site.register(Video, VideoAdmin)
--- videos/models.py
+++ videos/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Video(models.Model):
     title       = models.CharField(max_length=120)
     embed_code  = models.TextField()
+    updated     = models.DateTimeField(auto_now=True)
     timestamp   = models.DateTimeField(auto_now_add=True)
 
 
