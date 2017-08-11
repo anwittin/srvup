81ebdc4c424ac22818658790da6c6884bb2fab10
Tony Wittinger <anwittin@gmail.com>
2017-08-10 21:03:58 -0700

    update model

--- videos/models.py
+++ videos/models.py
@@ -3,8 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Video(models.Model):
-    embed_code = models.TextField()
+    title       = models.CharField(max_length=120)
+    embed_code  = models.TextField()
+    timestamp   = models.DateTimeField(auto_now_add=True)
 
 
     def __str__(self):
-        return self.embed_code
+        return self.title
